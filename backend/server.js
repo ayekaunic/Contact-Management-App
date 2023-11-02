e3095a400ff939830a442cb191291f459c05deae
Muhammad Aun <ayekaunic@gmail.com>
2023-11-02 15:34:56 +0500

    Created an Express server.

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,11 @@
-console.log("hello, world!");
+const express = require("express");
+const dotenv = require("dotenv").config();
+
+const app = express();
+
+const port = process.env.PORT || 3663;
+
+app.listen(
+    port,
+    () => console.log(`Server running on port ${port}`),
+);
