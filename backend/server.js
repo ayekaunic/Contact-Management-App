094af69e56c2d542458f8a10af610b196d4a6b3d
Muhammad Aun <ayekaunic@gmail.com>
2023-11-03 19:35:45 +0500

    oi bruv, moosh init?

--- backend/server.js
+++ backend/server.js
@@ -7,8 +7,8 @@ const app = express();
 const port = process.env.PORT || 3663;
 
 app.use(express.json());
-app.use(errorHandler);
 app.use("/api/contacts", require("./routes/contactRoutes"));
+app.use(errorHandler);
 
 app.listen(
     port,
