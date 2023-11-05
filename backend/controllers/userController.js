45cd20b90a65264484736e3959495f7f1e204abf
Muhammad Aun <ayekaunic@gmail.com>
2023-11-05 18:02:14 +0500

    User Registration & Password Hashing.

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,10 +1,36 @@
 const asyncHandler = require("express-async-handler");
-// const Contact = require("../models/contactModel");
+const bcrypt = require("bcrypt");
+const User = require("../models/userModel");
 
 //@desc Register a user
 //@route POST /api/users/register
 //@access public
 const registerUser = asyncHandler(async (req, res) => {
+    const { name, email, password } = req.body;
+    if (!name || !email || !password) {
+        res.status(400);
+        throw new Error("Please fill all the fields!");
+    }
+    const userAvailable = await User.findOne({ email });
+    if (userAvailable) {
+        res.status(400);
+        throw new Error("Email address already taken!");
+    }
+    // hash password
+    const hashedPassword = await bcrypt.hash(password, 10);
+    console.log("hashed password: ", hashedPassword);
+    const user = await User.create({
+        name,
+        email,
+        password: hashedPassword,
+    });
+    console.log(`User created:\n${user}`);
+    if (user) {
+        res.status(201).json({ _id: user.id, email: user.email });
+    } else {
+        res.status(400);
+        throw new Error("Invalid user data!");
+    }
     res.json({message : "Register the user."});
 });
 
---
+++ backend/models/userModel.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema({
+    name : {
+        type: String,
+        required: [true, "Please add the username."],
+    },
+    email : {
+        type: String,
+        required: [true, "Please add the email address."],
+        unique: [true, "Email address already taken."],
+    },
+    password : {
+        type: String,
+        required: [true, "Please add the password."],
+    },
+}, {
+    timestamps: true,
+});
+
+module.exports = mongoose.model("User", userSchema);
