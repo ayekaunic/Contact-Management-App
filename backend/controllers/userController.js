ebcc673b530a13a4e17046c8661f7cd7ceab22e8
Muhammad Aun <ayekaunic@gmail.com>
2023-11-05 15:33:43 +0500

    Added User Controllers.

---
+++ backend/controllers/userController.js
@@ -0,0 +1,29 @@
+const asyncHandler = require("express-async-handler");
+// const Contact = require("../models/contactModel");
+
+//@desc Register a user
+//@route POST /api/users/register
+//@access public
+const registerUser = asyncHandler(async (req, res) => {
+    res.json({message : "Register the user."});
+});
+
+//@desc Log in a user
+//@route POST /api/users/login
+//@access public
+const loginUser = asyncHandler(async (req, res) => {
+    res.json({message : "Login the user."});
+});
+
+//@desc Get current user
+//@route GET /api/users/current
+//@access private
+const currentUser = asyncHandler(async (req, res) => {
+    res.json({message : "Current user information."});
+});
+
+module.exports = {
+    registerUser,
+    loginUser,
+    currentUser,
+}
